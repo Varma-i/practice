daf844d6b39b9568e05039fec91f6e7c99361a03
varma <yashlongbeach@gmail.com>
2024-10-12 01:21:56 -0400

    idi malli practice chestunna urkane

---
+++ tej.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p " telugu best film maker " director
+
+if [[ $director == "rgv" ]];
+
+then
+	echo " his best film is raktacharitra "
+	
+else
+	echo " no its not him "
+fi
+
